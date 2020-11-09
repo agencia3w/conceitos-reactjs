16cf76ed052395a623c1740addfc817761183c59
Paulinho <paulinho@agencia3w.com.br>
2020-11-09 10:42:36 -0300

    Desafio ReactJS

--- src/App.js
+++ src/App.js
@@ -1,31 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+	const [repositories, setRepositories] = useState([]);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+	useEffect(() => {
+		api.get('/repositories').then(response => {
+			setRepositories(response.data);
+		})
+	}, []);
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+	async function handleAddRepository() {
+		const response = await api.post('/repositories', {
+			title: `Desafio Node.js com React ${Date.now()}`,
+			url: "http://github.com/agencia3w",
+			techs: ["Node.js", "JS"]
+		});
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+		const repository = response.data
+
+		setRepositories([...repositories, repository]);
+		console.log(repositories);
+	}
+
+	async function handleRemoveRepository(id) {
+		const newRepositories = repositories.filter(repository => repository.id != id);
+		await api.delete(`/repositories/${id}`);
+		setRepositories(newRepositories);
+	}
+
+	return (
+		<div>
+			<ul data-testid="repository-list">
+				{repositories.map(repository => (
+					<li key={repository.id}>
+						{repository.title}
+						<button onClick={() => handleRemoveRepository(repository.id)}>
+							Remover
+          				</button>
+					</li>
+				))}
+			</ul>
+
+			<button onClick={handleAddRepository}>Adicionar</button>
+		</div>
+	);
 }
 
 export default App;
